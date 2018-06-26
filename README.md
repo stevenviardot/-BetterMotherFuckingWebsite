a0954e9902763dc28d626ac5eee26b52bd63b52b
viardot steven <steven@MacBook-Air-de-viardot.local>
2018-06-26 18:23:40 +0200

    Version JS-2

---
+++ README.md
@@ -0,0 +1,4 @@
+Semaine 0 / Mercredi
+
+Lien : https://stevenviardot.github.io/BetterMotherFuckingWebsite/
+
--- main.js
+++ main.js
@@ -2,3 +2,21 @@ setTimeout(x, 10000);
 function x() {
   alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
 }
+
+ function y() {
+  document.getElementById('body').style.background = 'magenta';
+  var x = document.getElementById('body');
+  var yo = x.getElementsByTagName('P');
+  var i;
+  for (i = 0; i < yo.length; i++) {
+        yo[i].style.color = "red";
+    }
+ }
+
+ // function green() {
+ //    var xx = document.getElementById("title");
+ //    var i;
+ //    for (i = 0; i < x.length; i++) {
+ //        xx[i].style.color = "green";
+ //    }
+ // }
